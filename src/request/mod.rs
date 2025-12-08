0cb099d4a8e900f5bb1cd3b357a6c7997f54518c
pawsengineer <manovisut.ktp@gmail.com>
2025-12-08 09:25:13 +0700

    feat(data): add query parameters for trades and activities with pagination and sorting

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,3 +1,5 @@
+mod data_params;
 mod pagination;
 
+pub use data_params::{ActivityQueryParams, ActivitySortBy, SortDirection, TradeQueryParams};
 pub use pagination::{PaginationParams, END_CURSOR, INITIAL_CURSOR};
